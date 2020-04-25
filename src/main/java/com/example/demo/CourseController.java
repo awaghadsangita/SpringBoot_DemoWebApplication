9617daed924555574ca8acda89f33d9f78322414
sangita avghad <sangita.awaghad@gmail.com>
2020-04-24 20:19:02 -0700

    REFACTORED:removed HttpSession and used ModelAndView

--- src/main/java/com/example/demo/CourseController.java
+++ src/main/java/com/example/demo/CourseController.java
@@ -1,15 +1,17 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CourseController {
 	@RequestMapping("courses")
-	public String coures(String cname,HttpSession session) {
-		session.setAttribute("cname", cname);
-		return "Coureses";
+	public ModelAndView coures(@RequestParam("cname")String coursename) {
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("cname",coursename);
+		mv.setViewName("Coureses");
+		return mv;	
 	}
 }
