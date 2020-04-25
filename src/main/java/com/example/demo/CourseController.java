e6364a76fea8eef8f0bcae3f593eef400ca1b2f0
sangita avghad <sangita.awaghad@gmail.com>
2020-04-24 19:17:02 -0700

    REFACTORED:remove HttpServletRequest

--- src/main/java/com/example/demo/CourseController.java
+++ src/main/java/com/example/demo/CourseController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -9,11 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CourseController {
 	@RequestMapping("courses")
-	public String coures(HttpServletRequest req) {
-		HttpSession session=req.getSession();
-		String cname=req.getParameter("cname");
+	public String coures(String cname,HttpSession session) {
 		session.setAttribute("cname", cname);
-		System.out.println("Wel-come to Bridgelabz"+cname);
 		return "Coureses";
 	}
 }
